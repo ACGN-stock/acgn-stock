1bdade7b53e90f3055a002105e9978ecd4fb6ff3
shibale <shibale9712@gmail.com>
2017-07-28 21:59:40 +0800

    為資料庫建立索引，並整合進migrate中。

--- db/migrate.js
+++ db/migrate.js
@@ -1,20 +1,72 @@
 'use strict';
 import { Meteor } from 'meteor/meteor';
 import { Migrations } from 'meteor/percolate:migrations';
-// import { dbCompanies } from './dbCompanies';
+import { dbCompanies } from './dbCompanies';
+import { dbDirectors } from './dbDirectors';
+import { dbFoundations } from './dbFoundations';
+import { dbInstantMessage } from './dbInstantMessage';
+import { dbLog } from './dbLog';
+import { dbOrders } from './dbOrders';
+import { dbProducts } from './dbProducts';
+import { dbValidatingUsers } from './dbValidatingUsers';
 
 if (Meteor.isServer) {
-  // Migrations.add({
-  //   version: 1,
-  //   name: 'Create indexes.',
-  //   up() {
-  //     dbCompanies.rawCollection().createIndex({
-  //       name: 1
-  //     }, {
-  //       unique: true
-  //     });
-  //   }
-  // });
+  Migrations.add({
+    version: 1,
+    name: 'Create indexes.',
+    up() {
+      dbCompanies.rawCollection().createIndex({
+        companyName: 1
+      }, {
+        unique: true
+      });
+      dbCompanies.rawCollection().createIndex({
+        manager: 1
+      });
+      dbCompanies.rawCollection().createIndex({
+        tags: 1
+      });
+      dbCompanies.rawCollection().createIndex({
+        lastPrice: -1
+      });
+      dbCompanies.rawCollection().createIndex({
+        totalValue: -1
+      });
+      dbDirectors.rawCollection().createIndex({
+        companyName: 1,
+        username: 1
+      }, {
+        unique: true
+      });
+      dbFoundations.rawCollection().createIndex({
+        createdAt: 1
+      });
+      dbFoundations.rawCollection().createIndex({
+        companyName: 1
+      }, {
+        unique: true
+      });
+      dbInstantMessage.rawCollection().createIndex({
+        createdAt: -1
+      });
+      dbLog.rawCollection().createIndex({
+        createdAt: -1
+      });
+      dbOrders.rawCollection().createIndex({
+        orderType: 1,
+        unitPrice: 1
+      });
+      dbProducts.rawCollection().createIndex({
+        overdue: 1
+      });
+      dbValidatingUsers.rawCollection().createIndex({
+        username: 1,
+        password: 1
+      }, {
+        unique: true
+      });
+    }
+  });
 
   Meteor.startup(() => {
     Migrations.migrateTo('latest');
