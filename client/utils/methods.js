cb6721368e32e32ee509567455d1d3fafaa0fd2b
mrbigmouth <b92101007@gmail.com>
2017-08-27 00:34:59 +0800

    Update methods.js

--- client/utils/methods.js
+++ client/utils/methods.js
@@ -8,7 +8,7 @@ import { dbResourceLock } from '../../db/dbResourceLock';
 import { dbProductLike } from '../../db/dbProductLike';
 import { addTask, resolveTask } from '../layout/loading';
 import { handleError } from './handleError';
-import { AlertDialog } from '../layout/alertDialog';
+import { alertDialog } from '../layout/alertDialog';
 
 Meteor.subscribe('isChangingSeason');
 
@@ -16,12 +16,12 @@ Meteor.nativeCall = Meteor.call;
 Meteor.call = (function(_super) {
   function call(...args) {
     if (dbResourceLock.find('season').count()) {
-      AlertDialog.alert('伺服器正忙於商業季度的切換，請稍等一下吧！[503]');
+      alertDialog.alert('伺服器正忙於商業季度的切換，請稍等一下吧！[503]');
 
       return false;
     }
     if (! Meteor.status().connected) {
-      AlertDialog.alert('糟了，伺服器好像掛了！等一下再試試吧！[503]');
+      alertDialog.alert('糟了，伺服器好像掛了！等一下再試試吧！[503]');
 
       return false;
     }
@@ -59,7 +59,7 @@ export function createBuyOrder(user, companyData) {
     orderType: '賣出'
   });
   if (existsSellOrder) {
-    AlertDialog.alert('您有賣出該公司股票的訂單正在執行中，無法同時下達購買的訂單！');
+    alertDialog.alert('您有賣出該公司股票的訂單正在執行中，無法同時下達購買的訂單！');
 
     return false;
   }
@@ -67,12 +67,12 @@ export function createBuyOrder(user, companyData) {
   const minimumUnitPrice = Math.max(Math.ceil(companyData.listPrice / 2), 1);
   const maximumUnitPrice = Math.min(userMoney, companyData.listPrice * 2);
   if (minimumUnitPrice > maximumUnitPrice) {
-    AlertDialog.alert('您的金錢不足以購買此公司的股票！');
+    alertDialog.alert('您的金錢不足以購買此公司的股票！');
 
     return false;
   }
 
-  AlertDialog.dialog({
+  alertDialog.dialog({
     type: 'prompt',
     title: '股份購入',
     message: `請輸入您期望購入的每股單價：(${minimumUnitPrice}~${maximumUnitPrice})`,
@@ -82,17 +82,17 @@ export function createBuyOrder(user, companyData) {
         return false;
       }
       if (unitPrice < minimumUnitPrice || unitPrice > maximumUnitPrice) {
-        AlertDialog.alert('不正確的價格設定！');
+        alertDialog.alert('不正確的價格設定！');
 
         return false;
       }
       const maximumAmount = Math.floor(userMoney / unitPrice);
       if (maximumAmount < 1) {
-        AlertDialog.alert('您的金錢不足以購買此公司的股票！');
+        alertDialog.alert('您的金錢不足以購買此公司的股票！');
 
         return false;
       }
-      AlertDialog.dialog({
+      alertDialog.dialog({
         type: 'prompt',
         title: '股份購入',
         message: `請輸入總購入數量：(1~${maximumAmount})`,
@@ -102,7 +102,7 @@ export function createBuyOrder(user, companyData) {
             return false;
           }
           if (amount < 1 || amount > maximumAmount) {
-            AlertDialog.alert('不正確的數量設定！');
+            alertDialog.alert('不正確的數量設定！');
 
             return false;
           }
@@ -122,13 +122,13 @@ export function createSellOrder(user, companyData) {
     orderType: '購入'
   });
   if (existsBuyOrder) {
-    AlertDialog.alert('您有買入該公司股票的訂單正在執行中，無法同時下達賣出的訂單！');
+    alertDialog.alert('您有買入該公司股票的訂單正在執行中，無法同時下達賣出的訂單！');
 
     return false;
   }
   const minimumUnitPrice = Math.max(Math.ceil(companyData.listPrice / 2), 1);
   const maximumUnitPrice = companyData.listPrice * 2;
-  AlertDialog.dialog({
+  alertDialog.dialog({
     type: 'prompt',
     title: '股份賣出',
     message: `請輸入您期望賣出的每股單價：(${minimumUnitPrice}~${maximumUnitPrice})`,
@@ -138,13 +138,13 @@ export function createSellOrder(user, companyData) {
         return false;
       }
       if (unitPrice < minimumUnitPrice || unitPrice > maximumUnitPrice) {
-        AlertDialog.alert('不正確的價格設定！');
+        alertDialog.alert('不正確的價格設定！');
 
         return false;
       }
       const directorData = dbDirectors.findOne({userId, companyId});
       const maximumAmount = directorData.stocks;
-      AlertDialog.dialog({
+      alertDialog.dialog({
         type: 'prompt',
         title: '股份賣出',
         message: `請輸入總賣出數量：(1~${maximumAmount})`,
@@ -154,7 +154,7 @@ export function createSellOrder(user, companyData) {
             return false;
           }
           if (amount < 1 || amount > maximumAmount) {
-            AlertDialog.alert('不正確的數量設定！');
+            alertDialog.alert('不正確的數量設定！');
 
             return false;
           }
@@ -172,7 +172,7 @@ export function retrieveOrder(orderData) {
       '確定要取消「以$' + orderData.unitPrice +
       '單價' + orderData.orderType + '數量' + orderData.amount + '的「' +
       companyData.companyName + '」公司股份」這筆訂單嗎？（將付出手續費$1）';
-    AlertDialog.confirm(message, function(result) {
+    alertDialog.confirm(message, function(result) {
       if (result) {
         Meteor.call('retrieveOrder', orderData._id);
       }
@@ -181,12 +181,12 @@ export function retrieveOrder(orderData) {
 }
 
 export function changeChairmanTitle(companyData) {
-  AlertDialog.prompt('要修改董事長的頭銜嗎？', function(chairmanTitle) {
+  alertDialog.prompt('要修改董事長的頭銜嗎？', function(chairmanTitle) {
     if (chairmanTitle && chairmanTitle.length > 0 && chairmanTitle.length <= 20) {
       Meteor.call('changeChairmanTitle', companyData._id, chairmanTitle);
     }
     else if (chairmanTitle) {
-      AlertDialog.alert('無效的頭銜名稱！');
+      alertDialog.alert('無效的頭銜名稱！');
     }
   }, companyData.chairmanTitle);
 }
@@ -194,16 +194,16 @@ export function changeChairmanTitle(companyData) {
 export function voteProduct(productId) {
   const user = Meteor.user();
   if (! user) {
-    AlertDialog.alert('您尚未登入，無法向產品投推薦票！');
+    alertDialog.alert('您尚未登入，無法向產品投推薦票！');
 
     return false;
   }
   if (user.profile.vote < 1) {
-    AlertDialog.alert('您的推薦票數量不足，無法繼續推薦產品！');
+    alertDialog.alert('您的推薦票數量不足，無法繼續推薦產品！');
 
     return false;
   }
-  AlertDialog.confirm('您的推薦票剩餘' + user.profile.vote + '張，確定要向產品投出推薦票嗎？', function(result) {
+  alertDialog.confirm('您的推薦票剩餘' + user.profile.vote + '張，確定要向產品投出推薦票嗎？', function(result) {
     if (result) {
       Meteor.call('voteProduct', productId);
     }
@@ -213,19 +213,19 @@ export function voteProduct(productId) {
 export function likeProduct(productId, companyId) {
   const user = Meteor.user();
   if (! user) {
-    AlertDialog.alert('您尚未登入，無法向產品進行董事推薦！');
+    alertDialog.alert('您尚未登入，無法向產品進行董事推薦！');
 
     return false;
   }
   const companyData = dbCompanies.findOne(companyId);
   const userId = user._id;
   if (dbDirectors.find({companyId, userId}).count() < 1) {
-    AlertDialog.alert('您至少需要擁有一張「' + companyData.companyName + '」的股票才可對公司產品進行董事推薦！');
+    alertDialog.alert('您至少需要擁有一張「' + companyData.companyName + '」的股票才可對公司產品進行董事推薦！');
 
     return false;
   }
   if (dbProductLike.find({productId, userId}).count() > 0) {
-    AlertDialog.confirm('您已經對此產品做出過正面評價，要收回評價嗎？', function(result) {
+    alertDialog.confirm('您已經對此產品做出過正面評價，要收回評價嗎？', function(result) {
       if (result) {
         Meteor.call('likeProduct', productId);
       }
