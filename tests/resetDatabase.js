56b627d7dde54e94700274ceac3630ea95ed03a7
mrbigmouth <b92101007@gmail.com>
2017-09-10 10:04:06 +0800

    調整資料庫重設程式。

--- tests/resetDatabase.js
+++ tests/resetDatabase.js
@@ -15,84 +15,14 @@ import { dbRankUserWealth } from '../db/dbRankUserWealth';
 import { dbResourceLock } from '../db/dbResourceLock';
 import { dbSeason } from '../db/dbSeason';
 import { dbValidatingUsers } from '../db/dbValidatingUsers';
+import { dbVoteRecord } from '../db/dbVoteRecord';
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(function() {
-  const now = new Date();
-  dbCompanies.find().forEach((companyData) => {
-    const companyName = companyData.companyName;
-    if (dbFoundations.findOne({companyName})) {
-      console.log('not insert company[' + companyName + '] because alread have in foundations.');
-    }
-    else {
-      console.log('insert company[' + companyName + '] back into foundations.');
-      dbFoundations.insert({
-        companyName: companyName,
-        manager: companyData.manager,
-        tags: companyData.tags,
-        pictureSmall: companyData.pictureSmall,
-        pictureBig: companyData.pictureBig,
-        description: companyData.description,
-        invest: [],
-        createdAt: now
-      });
-    }
-  });
   dbAdvertising.remove({});
   dbCompanies.remove({});
   dbDirectors.remove({});
-  const startFoundationTime = 1503547200000;
-  dbFoundations
-    .find({}, {
-      field: {
-        _id: 1,
-        manager: 1
-      }
-    })
-    .forEach((foundationData, index) => {
-      const timeDiff = 3900000 * Math.floor(index / 10);
-      const createdAt = new Date(startFoundationTime + timeDiff);
-      console.log('update foundation created at: ' + createdAt);
-      dbFoundations.update(foundationData._id, {
-        $set: {
-          createdAt: createdAt
-        }
-      });
-    });
-  dbFoundations.update(
-    {},
-    {
-      $set: {
-        invest: []
-      }
-    },
-    {
-      multi: true
-    }
-  );
-  let stoneCount = 0;
-  Meteor.users.find().forEach((userData) => {
-    const userId = userData._id;
-    const logDataCursor = dbLog.find({
-      userId: userId,
-      logType: {
-        $nin: ['驗證通過', '發薪紀錄', '創立成功', '創立失敗', '創立得股', '創立退款', '免費得石']
-      }
-    });
-    if (logDataCursor.count() > 0) {
-      console.log('find user[' + userId + '] log data, increase stone by 1.');
-      Meteor.users.update(userId, {
-        $inc: {
-          'profile.stone': 1
-        }
-      });
-      stoneCount += 1;
-    }
-    else {
-      console.log('user[' + userData.username + '] don\'t have log data.');
-    }
-  });
-  console.log('total give ' + stoneCount + ' stones to ' + stoneCount + ' users!');
+  dbFoundations.remove({});
   dbLog.remove({});
   dbOrders.remove({});
   dbPrice.remove({});
@@ -108,6 +38,9 @@ Meteor.startup(function() {
   Meteor.users.update(
     {},
     {
+      $inc: {
+        'profile.stone': 1
+      },
       $set: {
         'profile.money': 10000
       }
@@ -116,6 +49,7 @@ Meteor.startup(function() {
       multi: true
     }
   );
+  dbVoteRecord.remove({});
   const date1 = new Date();
   const date2 = new Date(date1.getTime() + 1);
   Meteor.users.find({}).forEach((user) => {
