0fe0d3ac53c30cbf5f32a4536c2b02a99cff59e7
silentvow <silentvow@gmail.com>
2017-09-14 16:16:36 +0800

    轉移暗色主題css至styles.css。

--- client/layout/layout.js
+++ client/layout/layout.js
@@ -5,6 +5,15 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { rShowLoginDialog } from './validateDialog';
 import { rShowAlertDialog } from './alertDialog';
 
+export const rMainTheme = new ReactiveVar('light');
+
+//每次開啟網頁時只確認一次預設theme
+if (! localStorage.getItem('theme')) {
+  localStorage.setItem('theme', 'light');
+}
+Template.layout.onCreated(function() {
+  rMainTheme.set(localStorage.getItem('theme'));
+});
 Template.layout.helpers({
   currentPage() {
     return FlowRouter.getRouteName();
@@ -14,6 +23,12 @@ Template.layout.helpers({
   },
   showAlertDialog() {
     return rShowAlertDialog.get();
+  },
+  containerClass() {
+    if (rMainTheme.get() === 'light') {
+      return 'container container-light';
+    }
+    return 'container container-dark';
   }
 });
 
