f71b92407cc8b515511f1940e6d634e24f8d66b1
bob.lee <bob.lee@yesee.com.tw>
2017-09-22 11:33:05 +0800

    防止本機測試時被轉到ssl。

--- client/utils/ssl.js
+++ client/utils/ssl.js
@@ -1,3 +1,6 @@
-if (location.protocol === 'http:') {
+'use strict';
+import { Meteor } from 'meteor/meteor';
+
+if (Meteor.isProduction && location.protocol === 'http:') {
   location.href = location.href.replace('http:', 'https:');
 }
