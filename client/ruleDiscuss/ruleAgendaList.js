544bbd42c81a9fe7d10c0bc46549e5f952c76007
silentvow <silentvow@gmail.com>
2017-10-09 03:27:01 +0800

    議程列表頁面訂閱。

--- client/ruleDiscuss/ruleAgendaList.js
+++ client/ruleDiscuss/ruleAgendaList.js
@@ -5,7 +5,7 @@ import { $ } from 'meteor/jquery';
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-import { dbAdvertising } from '../../db/dbAdvertising';
+import { dbRuleAgendas } from '../../db/dbRuleAgendas';
 import { inheritedShowLoadingOnSubscribing } from '../layout/loading';
 import { inheritUtilForm, handleInputChange as inheritedHandleInputChange } from '../utils/form';
 import { formatDateText } from '../utils/helpers';
@@ -20,7 +20,7 @@ Template.ruleAgendaList.onCreated(function() {
     if (shouldStopSubscribe()) {
       return false;
     }
-    // this.subscribe('allAdvertising');
+    this.subscribe('allRuleAgenda');
   });
 });
 Template.ruleAgendaList.helpers({
@@ -28,27 +28,54 @@ Template.ruleAgendaList.helpers({
     return FlowRouter.path('createRuleAgenda');
   },
   getAgendaHref(agendaId) {
-    return FlowRouter.path('ruleAgenda') + '/' + agendaId;
+    return FlowRouter.path('ruleAgendaDetail', {agendaId});
   },
   processingAgendaList() {
-    return [];
+    const currentDate = Date.now();
+    const agendaList = [];
+    dbRuleAgendas.find({}, {
+      sort: {
+        createdAt: -1
+      }
+    }).forEach((agenda) => {
+      const expireDate = new Date(agenda.createdAt.getTime() + (agenda.duration * 60 * 60 * 1000));
+      if (expireDate >= currentDate) {
+        agendaList.push(agenda);
+      }
+    });
+    console.log('processingAgendaList', dbRuleAgendas.find({}).fetch());
+    return agendaList;
   },
   finishAgendaList() {
-    return [];
+    const currentDate = Date.now();
+    const agendaList = [];
+    dbRuleAgendas.find({}, {
+      sort: {
+        createdAt: -1
+      }
+    }).forEach((agenda) => {
+      const expireDate = new Date(agenda.createdAt.getTime() + (agenda.duration * 60 * 60 * 1000));
+      if (expireDate < currentDate) {
+        agendaList.push(agenda);
+      }
+    });
+    return agendaList;
   },
   formatExpireDate(agenda) {
-    return Date.now();
+    const expireDate = new Date(agenda.createdAt.getTime() + (agenda.duration * 60 * 60 * 1000));
+    
+    return formatDateText(expireDate);
   }
 });
 
 Template.ruleAgendaList.events({
   'click [data-take-down]'(event) {
     event.preventDefault();
-    const advertisingId = $(event.currentTarget).attr('data-take-down');
-    const advertisingData = dbAdvertising.findOne(advertisingId);
-    alertDialog.confirm('確定要撤銷議程「' + advertisingData.message + '」？', (result) => {
+    const agendaId = $(event.currentTarget).attr('data-take-down');
+    const agendaData = dbRuleAgendas.findOne(agendaId);
+    alertDialog.confirm('確定要撤銷議程「' + agendaData.title + '」？', (result) => {
       if (result) {
-        Meteor.customCall('takeDownAdvertising', advertisingId);
+        Meteor.customCall('takeDownRuleAgenda', agendaId);
       }
     });
   }
