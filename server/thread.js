b8f9b70c73a5a5c6d8ad2edb4ed5985d450f0644
mrbigmouth <b92101007@gmail.com>
2017-10-10 12:40:47 +0800

    重用應重複使用的程式碼。

--- server/thread.js
+++ server/thread.js
@@ -6,12 +6,7 @@ import { dbThreads } from '../db/dbThreads';
 export const threadId = (process.env.GALAXY_CONTAINER_ID || '') + '!' + process.pid;
 
 Meteor.startup(function() {
-  dbThreads.insert({
-    _id: threadId,
-    doIntervalWork: false,
-    refreshTime: new Date(),
-    connections: UserStatus.connections.find().count()
-  });
+  refreshThread();
   //定期更新thread回報時間與當前連線數量
   Meteor.setInterval(refreshThread, 15000);
 });
