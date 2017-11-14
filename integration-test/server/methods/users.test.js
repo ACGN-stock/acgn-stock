895c393a4724e71470de96fa4421eb45bbb69fed
Gerald Du <frozenmouse@gmail.com>
2017-11-15 01:33:59 +0800

    加入巴哈帳號認證的測試、重構 validateBahamutAccount、修整PTT帳號認證的測試

--- integration-test/server/methods/users.test.js
+++ integration-test/server/methods/users.test.js
@@ -14,6 +14,10 @@ import '/server/methods/users';
 mustSinon(expect);
 
 describe('method loginOrRegister', function() {
+  function loginOrRegister(params) {
+    return promisify(Meteor.call)('loginOrRegister', params);
+  }
+
   const userData = {
     username: 'someone',
     password: 'mypass',
@@ -35,7 +39,7 @@ describe('method loginOrRegister', function() {
     });
 
     it('should return true', function() {
-      return promisify(Meteor.call)('loginOrRegister', {
+      return loginOrRegister({
         username: userData.username,
         password: userData.password,
         type: userData.profile.validateType,
@@ -46,7 +50,7 @@ describe('method loginOrRegister', function() {
     it('should return a validation code and save the user data to validatingUsers when the user wants to reset the password', function() {
       const newPassword = 'newPassword';
 
-      return promisify(Meteor.call)('loginOrRegister', {
+      return loginOrRegister({
         username: userData.username,
         password: newPassword,
         type: userData.profile.validateType,
@@ -67,7 +71,7 @@ describe('method loginOrRegister', function() {
 
   describe('when the user does not exist', function() {
     it('should return a validation code and save the user data to validatingUsers', function() {
-      return promisify(Meteor.call)('loginOrRegister', {
+      return loginOrRegister({
         username: userData.username,
         password: userData.password,
         type: userData.profile.validateType,
@@ -86,18 +90,16 @@ describe('method loginOrRegister', function() {
     });
 
     it('should return the same validation code across different calls', function() {
-      const methodParams = [
-        {
-          username: userData.username,
-          password: userData.password,
-          type: userData.profile.validateType,
-          reset: false
-        }
-      ];
+      const params = {
+        username: userData.username,
+        password: userData.password,
+        type: userData.profile.validateType,
+        reset: false
+      };
 
       return Promise.all([
-        promisify(Meteor.call)('loginOrRegister', ...methodParams),
-        promisify(Meteor.call)('loginOrRegister', ...methodParams)
+        loginOrRegister(params),
+        loginOrRegister(params)
       ])
         .then(([result1, result2]) => {
           expect(result1).to.equal(result2);
@@ -107,6 +109,10 @@ describe('method loginOrRegister', function() {
 });
 
 describe('method validatePTTAccount', function() {
+  function validatePTTAccount(username) {
+    return promisify(Meteor.call)('validatePTTAccount', username);
+  }
+
   const correctValidateCode = 'abcdefghij';
   const wrongValidateCode = 'zxcvbasdfg';
 
@@ -117,10 +123,12 @@ describe('method validatePTTAccount', function() {
   beforeEach(function() {
     resetDatabase();
     sinon.stub(HTTP, 'get');
+    sinon.spy(Accounts, 'setPassword');
   });
 
   afterEach(function() {
     HTTP.get.restore();
+    Accounts.setPassword.restore();
   });
 
   it('should also validate other users', function() {
@@ -140,67 +148,19 @@ describe('method validatePTTAccount', function() {
       `
     });
 
-    return promisify(Meteor.call)('validatePTTAccount', 'otherPttUser')
-      .must.reject.with.an.error(Meteor.Error)
+    return validatePTTAccount('otherPttUser').must.reject.with.an.error(Meteor.Error)
       .then(() => {
         expect(dbValidatingUsers.findOne({ username })).to.not.exist();
-        expect(Accounts.findUserByUsername(username)).to.exist();
+        expect(Meteor.users.findOne({ username })).to.exist();
       });
   });
 
-  describe('when the user does not exist', function() {
-    it('should throw error if there is no pending validation for the user', function() {
-      return promisify(Meteor.call)('validatePTTAccount', username)
-        .must.reject.with.an.error(Meteor.Error);
+  describe('without pending validation', function() {
+    it('should throw error if the user does not exist', function() {
+      return validatePTTAccount(username).must.reject.with.an.error(Meteor.Error);
     });
 
-    describe('if the user has a pending validation', function() {
-      beforeEach(function() {
-        dbValidatingUsers.insert({
-          username,
-          password: currentPassword,
-          validateCode: correctValidateCode,
-          createdAt: new Date()
-        });
-      });
-
-      it('should throw error if the validation code in the article does not match', function() {
-        HTTP.get.returns({
-          content: `
-            <div class="push">
-              <span class="push-userid">${username}</span>
-              <span class="push-content">: ${wrongValidateCode}</span>
-            </div>
-          `
-        });
-
-        return promisify(Meteor.call)('validatePTTAccount', username)
-          .must.reject.with.an.error(Meteor.Error);
-      });
-
-      it('should create a new user if the validation passed', function() {
-        HTTP.get.returns({
-          content: `
-            <div class="push">
-              <span class="push-userid">${username}</span>
-              <span class="push-content">: ${correctValidateCode}</span>
-            </div>
-          `
-        });
-
-        expect(Accounts.findUserByUsername(username)).to.not.exist();
-
-        return promisify(Meteor.call)('validatePTTAccount', username)
-          .must.resolve.with.true()
-          .then(() => {
-            expect(Accounts.findUserByUsername(username)).to.exist();
-          });
-      });
-    });
-  });
-
-  describe('when the user exists', function() {
-    beforeEach(function() {
+    it('should return true if the user already exists', function() {
       Accounts.createUser({
         username,
         password: currentPassword,
@@ -209,60 +169,200 @@ describe('method validatePTTAccount', function() {
           validateType: 'PTT'
         }
       });
+
+      return validatePTTAccount(username).must.resolve.with.true();
+    });
+  });
+
+  describe('with pending validation', function() {
+    beforeEach(function() {
+      dbValidatingUsers.insert({
+        username,
+        password: newPassword,
+        validateCode: correctValidateCode,
+        createdAt: new Date()
+      });
     });
 
-    it('should return true when there is no pending validation for the user', function() {
-      return promisify(Meteor.call)('validatePTTAccount', username)
-        .must.resolve.with.true();
+    it('should throw error if the user pushed no validation code', function() {
+      HTTP.get.returns({ content: '' });
+
+      return validatePTTAccount(username).must.reject.with.an.error(Meteor.Error);
     });
 
-    describe('if the user has a pending validation', function() {
-      beforeEach(function() {
-        dbValidatingUsers.insert({
-          username,
-          password: newPassword,
-          validateCode: correctValidateCode,
-          createdAt: new Date()
+    it('should throw error if the user pushed wrong validation code', function() {
+      HTTP.get.returns({
+        content: `
+          <div class="push">
+            <span class="push-userid">${username}</span>
+            <span class="push-content">: ${wrongValidateCode}</span>
+          </div>
+        `
+      });
+
+      return validatePTTAccount(username).must.reject.with.an.error(Meteor.Error);
+    });
+
+    it('should create a new user if the validation passed and the user does not exist', function() {
+      HTTP.get.returns({
+        content: `
+          <div class="push">
+            <span class="push-userid">${username}</span>
+            <span class="push-content">: ${correctValidateCode}</span>
+          </div>
+        `
+      });
+
+      return validatePTTAccount(username).must.resolve.with.true()
+        .then(() => {
+          expect(Meteor.users.find({ username })).to.exist();
         });
+    });
 
-        sinon.spy(Accounts, 'setPassword');
+    it('should reset user password if the validation passed and the user already exists', function() {
+      Accounts.createUser({
+        username,
+        password: currentPassword,
+        profile: {
+          name: username,
+          validateType: 'PTT'
+        }
       });
 
-      afterEach(function() {
-        Accounts.setPassword.restore();
+      HTTP.get.returns({
+        content: `
+          <div class="push">
+            <span class="push-userid">${username}</span>
+            <span class="push-content">: ${correctValidateCode}</span>
+          </div>
+        `
       });
 
-      it('should throw error if the validation code in the article does not match', function() {
-        HTTP.get.returns({
-          content: `
-            <div class="push">
-              <span class="push-userid">${username}</span>
-              <span class="push-content">: ${wrongValidateCode}</span>
-            </div>
-          `
+      return validatePTTAccount(username).must.resolve.with.true()
+        .then(() => {
+          const { _id: userId } = Meteor.users.findOne({ username });
+          Accounts.setPassword.must.have.been.calledWith(userId, newPassword);
         });
+    });
+  });
+});
 
-        return promisify(Meteor.call)('validatePTTAccount', username)
-          .must.reject.with.an.error(Meteor.Error);
+describe('method validateBahamutAccount', function() {
+  function validateBahamutAccount(username) {
+    return promisify(Meteor.call)('validateBahamutAccount', username);
+  }
+
+  const correctValidateCode = 'abcdefghij';
+  const wrongValidateCode = 'zxcvbasdfg';
+
+  const username = 'bahamutUser';
+  const checkUsername = `?${username}`;
+  const currentPassword = 'currentPassword';
+  const newPassword = 'newPassword';
+
+  const homeIndexUrl = `https://home.gamer.com.tw/homeindex.php?owner=${username}`;
+  const homeReplyListUrl = `https://home.gamer.com.tw/homeReplyList.php?owner=${username}`;
+
+  beforeEach(function() {
+    resetDatabase();
+    sinon.stub(HTTP, 'get');
+    sinon.spy(Accounts, 'setPassword');
+  });
+
+  afterEach(function() {
+    HTTP.get.restore();
+    Accounts.setPassword.restore();
+  });
+
+  describe('without pending validation', function() {
+    it('should throw error if the user does not exist', function() {
+      return validateBahamutAccount(username).must.reject.with.an.error(Meteor.Error);
+    });
+
+    it('should return true if the user exists', function() {
+      Accounts.createUser({
+        username: checkUsername,
+        password: currentPassword,
+        profile: {
+          name: username,
+          validateType: 'Bahamut'
+        }
       });
 
-      it('should reset user password if the validation passed', function() {
-        HTTP.get.returns({
-          content: `
-            <div class="push">
-              <span class="push-userid">${username}</span>
-              <span class="push-content">: ${correctValidateCode}</span>
-            </div>
-          `
+      return validateBahamutAccount(username).must.resolve.with.true();
+    });
+  });
+
+  describe('with pending validation', function() {
+    beforeEach(function() {
+      dbValidatingUsers.insert({
+        username: checkUsername,
+        password: newPassword,
+        validateCode: correctValidateCode,
+        createdAt: new Date()
+      });
+    });
+
+    it('should throw error if the user does not have phone validation info', function() {
+      HTTP.get.withArgs(homeIndexUrl).returns({ content: '' });
+
+      return validateBahamutAccount(username).must.reject.with.an.error(Meteor.Error);
+    });
+
+    it('should throw error if the user has passed phone validation but no validatation code reply found', function() {
+      HTTP.get
+        .withArgs(homeIndexUrl)
+        .returns({ content: '<li>手機認證：有</li>' })
+        .withArgs(homeReplyListUrl)
+        .returns({ content: '' });
+
+      return validateBahamutAccount(username).must.reject.with.an.error(Meteor.Error);
+    });
+
+    it('should throw error if the user has passed phone validation but replied wrong validatation code', function() {
+      HTTP.get
+        .withArgs(homeIndexUrl)
+        .returns({ content: '<li>手機認證：有</li>' })
+        .withArgs(homeReplyListUrl)
+        .returns({ content: `<span><a href="home.php?owner=${username}"></a>：${wrongValidateCode}</span>` });
+
+      return validateBahamutAccount(username).must.reject.with.an.error(Meteor.Error);
+    });
+
+    it('should create a new user if the validation process passed and the user does not exist', function() {
+      HTTP.get
+        .withArgs(homeIndexUrl)
+        .returns({ content: '<li>手機認證：有</li>' })
+        .withArgs(homeReplyListUrl)
+        .returns({ content: `<span><a href="home.php?owner=${username}"></a>：${correctValidateCode}</span>` });
+
+      return validateBahamutAccount(username).must.resolve.with.true()
+        .then(() => {
+          expect(Meteor.users.findOne({ username: checkUsername })).to.exist();
         });
+    });
 
-        return promisify(Meteor.call)('validatePTTAccount', username)
-          .must.resolve.with.true()
-          .then(() => {
-            const { _id: userId } = Meteor.users.findOne({ username });
-            Accounts.setPassword.must.have.been.calledWith(userId, newPassword);
-          });
+    it('should reset password if the validation process passed and the user already exists', function() {
+      Accounts.createUser({
+        username: checkUsername,
+        password: currentPassword,
+        profile: {
+          name: username,
+          validateType: 'Bahamut'
+        }
       });
+
+      HTTP.get
+        .withArgs(homeIndexUrl)
+        .returns({ content: '<li>手機認證：有</li>' })
+        .withArgs(homeReplyListUrl)
+        .returns({ content: `<span><a href="home.php?owner=${username}"></a>：${correctValidateCode}</span>` });
+
+      return validateBahamutAccount(username).must.resolve.with.true()
+        .then(() => {
+          const { _id: userId } = Meteor.users.findOne({ username: checkUsername });
+          Accounts.setPassword.must.have.been.calledWith(userId, newPassword);
+        });
     });
   });
 });
--- server/methods/users.js
+++ server/methods/users.js
@@ -116,60 +116,77 @@ function validateAllPTTAccounts() {
 Meteor.methods({
   validateBahamutAccount(username) {
     check(username, String);
-    validateBahamutAccount(username);
+
+    debug.log('validateBahamutAccount', username);
+    const checkUsername = `?${username}`;
+    const validatingUser = dbValidatingUsers.findOne({ username: checkUsername });
+
+    if (! validatingUser) {
+      const existingUser = Meteor.users.findOne({ username: checkUsername }, { fields: { _id: 1 } });
+
+      if (existingUser) {
+        return true;
+      }
+      else {
+        throw new Meteor.Error(404, '無法取得驗證碼，請重新註冊！');
+      }
+    }
+
+    if (! checkBahamutPhoneValidation(username)) {
+      throw new Meteor.Error(403, '您的巴哈姆特帳號尚未通過手機認證，請先通過手機認證之後再進行驗證！');
+    }
+
+    const { validateCode, password } = validatingUser;
+    if (! checkBahamutValidationCodeReply(username, validateCode)) {
+      throw new Meteor.Error(403, `無法查詢到驗證碼，請確定驗證碼[${validateCode}]是否輸入正確，且有出現在您的訪客留言頁面上！`);
+    }
+
+    const existingUser = Meteor.users.findOne({username: checkUsername}, { fields: { _id: 1 } });
+
+    if (existingUser) { // 既有帳號通過認證 → 重設密碼
+      Accounts.setPassword(existingUser._id, password, { logout: true });
+    }
+    else { // 新人通過認證 → 建立新帳號
+      Accounts.createUser({
+        username: checkUsername,
+        password,
+        profile: {
+          validateType: 'Bahamut',
+          name: username
+        }
+      });
+    }
+
+    dbValidatingUsers.remove(validatingUser._id);
+
+    return true;
   }
 });
 //一分鐘最多1次
 limitMethod('validateBahamutAccount', 1);
-function validateBahamutAccount(username) {
-  debug.log('validateBahamutAccount', username);
-  const checkUsername = '?' + username;
-  const validatingUser = dbValidatingUsers.findOne({username: checkUsername});
-  if (validatingUser) {
-    let url = 'https://home.gamer.com.tw/homeindex.php?owner=' + username;
-    let urlContent = HTTP.get(url).content;
-    let checkCannotPass = true;
-    cheerio.load(urlContent)('li').each((index, li) => {
-      const liChildren = li.children;
-      if (liChildren.length === 1 && (liChildren[0].data === '手機認證：有' || liChildren[0].data === '手機認證：永久')) {
-        checkCannotPass = false;
-      }
-    });
-    if (checkCannotPass) {
-      throw new Meteor.Error(403, '您的巴哈姆特帳號尚未通過手機認證，請先通過手機認證之後再進行驗證！');
-    }
-    url = 'https://home.gamer.com.tw/homeReplyList.php?owner=' + username;
-    urlContent = HTTP.get(url).content;
-    const userSayText = cheerio.load(urlContent)('a[href="home.php?owner=' + username + '"]')
-      .parent()
-      .text();
-    const validateCode = validatingUser.validateCode;
-    if (userSayText.indexOf('：' + validateCode) === -1) {
-      throw new Meteor.Error(403, '無法查詢到驗證碼，請確定驗證碼[' + validateCode + ']是否輸入正確，且有出現在您的訪客留言頁面上！');
-    }
-    const password = validatingUser.password;
-    const existUser = Meteor.users.findOne({username: checkUsername}, {
-      fields: {
-        _id: 1
-      }
-    });
-    if (existUser) {
-      Accounts.setPassword(existUser._id, password, {
-        logout: true
-      });
-      dbValidatingUsers.remove(validatingUser._id);
-    }
-    else {
-      const profile = {
-        validateType: 'Bahamut',
-        name: username
-      };
-      Accounts.createUser({username: checkUsername, password, profile});
-      dbValidatingUsers.remove(validatingUser._id);
-    }
-  }
 
-  return true;
+
+// 檢查巴哈小屋是否有通過手機認證的資訊
+function checkBahamutPhoneValidation(username) {
+  const { content } = HTTP.get(`https://home.gamer.com.tw/homeindex.php?owner=${username}`);
+
+  return cheerio.load(content)('li')
+    .filter((i, e) => {
+      const children = e.children;
+
+      return children.length === 1 && (children[0].data === '手機認證：有' || children[0].data === '手機認證：永久');
+    })
+    .length > 0;
+}
+
+// 檢查巴哈小屋的訪客留言是否有自己貼出的驗證碼
+function checkBahamutValidationCodeReply(username, validateCode) {
+  const { content } = HTTP.get(`https://home.gamer.com.tw/homeReplyList.php?owner=${username}`);
+
+  return cheerio.load(content)(`a[href="home.php?owner=${username}"]`)
+    .parent()
+    .text()
+    .indexOf(`：${validateCode}`) !== -1;
 }
 
 Accounts.validateNewUser((user) => {
