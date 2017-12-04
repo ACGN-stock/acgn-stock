66cd7d51e95a1fd968794dbe3e5ee8db48dfc118
Gerald Du <frozenmouse@gmail.com>
2017-12-04 22:16:20 +0800

    修正引入路徑

--- server/methods/company/markCompanyIllegal.js
+++ server/methods/company/markCompanyIllegal.js
@@ -2,8 +2,8 @@ import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 
 import { dbCompanies } from '/db/dbCompanies';
-import { limitMethod } from '/server/imports/rateLimit';
-import { debug } from '/server/imports/debug';
+import { limitMethod } from '/server/imports/utils/rateLimit';
+import { debug } from '/server/imports/utils/debug';
 
 Meteor.methods({
   markCompanyIllegal(companyId, reason) {
