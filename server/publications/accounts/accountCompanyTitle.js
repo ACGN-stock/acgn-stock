d8d1b2ce5910fa7ea566d2bcca4d50eb363c5995
Tsuki Akiba <erase2004@gmail.com>
2018-01-19 15:09:28 +0800

    1. 帳號資訊 > 員工，增加對已封鎖公司的標註顯示

--- server/publications/accounts/accountCompanyTitle.js
+++ server/publications/accounts/accountCompanyTitle.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { check, Match } from 'meteor/check';
 
 import { dbCompanies } from '/db/dbCompanies';
+import { dbEmployees } from '/db/dbEmployees';
 import { limitSubscription } from '/server/imports/utils/rateLimit';
 import { debug } from '/server/imports/utils/debug';
 import { publishTotalCount } from '/server/imports/utils/publishTotalCount';
@@ -11,7 +12,7 @@ Meteor.publish('accountChairmanTitle', function(userId, offset) {
   check(userId, String);
   check(offset, Match.Integer);
 
-  const filter = {chairman: userId};
+  const filter = {chairman: userId, isSeal: false};
 
   publishTotalCount('totalCountOfChairmanTitle', dbCompanies.find(filter), this);
 
@@ -46,7 +47,7 @@ Meteor.publish('accountManagerTitle', function(userId, offset) {
   check(userId, String);
   check(offset, Match.Integer);
 
-  const filter = {manager: userId};
+  const filter = {manager: userId, isSeal: false};
 
   publishTotalCount('totalCountOfManagerTitle', dbCompanies.find(filter), this);
 
@@ -75,3 +76,23 @@ Meteor.publish('accountManagerTitle', function(userId, offset) {
 });
 //一分鐘最多20次
 limitSubscription('accountManagerTitle');
+
+Meteor.publish('accounEmployeeTitle', function(userId) {
+  debug.log('publish accounEmployeeTitle', {userId});
+  check(userId, String);
+
+  const filter = {userId, resigned: false};
+
+  return [
+    dbEmployees.find(filter),
+    dbCompanies.find({
+      '_id': {
+        '$in': dbEmployees.find(filter).map((companyData) => {
+          return companyData.companyId;
+        })
+      }
+    })
+  ];
+});
+//一分鐘最多20次
+limitSubscription('accounEmployeeTitle');
