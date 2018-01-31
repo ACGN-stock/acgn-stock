c0309b1e3cdaf8b5febd673bcb57d78ff97ff4e7
Gerald Du <frozenmouse@gmail.com>
2018-01-31 22:25:01 +0800

    移除在賽季結束或公司被查封時退還所有石頭時產生的 log

--- server/functions/miningMachine/returnCompanyStones.js
+++ server/functions/miningMachine/returnCompanyStones.js
@@ -1,6 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 
-import { dbLog } from '/db/dbLog';
 import { dbCompanyStones } from '/db/dbCompanyStones';
 
 // 歸還公司中所有的石頭
@@ -12,27 +11,12 @@ export function returnCompanyStones(companyId) {
   }
 
   const userBulk = Meteor.users.rawCollection().initializeUnorderedBulkOp();
-  const logBulk = dbLog.rawCollection().initializeUnorderedBulkOp();
-  const logSchema = dbLog.simpleSchema();
-  const nowDate = new Date();
 
   companyStonesCursor.forEach(({ userId, stoneType }) => {
     userBulk.find({ _id: userId }).updateOne({ $inc: { [`profile.stones.${stoneType}`]: 1 } });
-
-    const logData = logSchema.clean({
-      logType: '礦機取石',
-      userId: [userId],
-      companyId,
-      data: { stoneType },
-      createdAt: nowDate
-    });
-    logSchema.validate(logData);
-
-    logBulk.insert(logData);
   });
 
-  Meteor.wrapAsync(userBulk.execute).call(userBulk);
-  Meteor.wrapAsync(logBulk.execute).call(logBulk);
+  Meteor.wrapAsync(userBulk.execute, userBulk)();
 
   dbCompanyStones.remove({ companyId });
 }
