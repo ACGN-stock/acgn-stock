abebd82bb879c3e56df8141b7717885763cd70b1
papago89 <papago2683@gmail.com>
2018-03-21 20:08:10 +0800

    將Markdown產生之<code>區塊內的跳脫字元反跳脫回來以使顯示正常化

--- client/utils/helpers.js
+++ client/utils/helpers.js
@@ -241,9 +241,22 @@ const katexExtension = {
   }
 };
 
+const preTagEscapedCharacterTranser = {
+  type: 'output',
+  filter: function(text) {
+    const output = text.replace(/<code>((.|\r|\n)*?)<\/code>/g, function(match, capture) {
+      const text = capture.replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&quot;/g, '"').replace(/&excl;/g, '!');
+
+      return `<code>${text}</code>`;
+    });
+
+    return output;
+  }
+};
+
 // Advance(KaTeX, image)
 export function markdown(content, disableAdvance = true) {
-  const extensionsArray = [xssFilter, footnotes];
+  const extensionsArray = [xssFilter, footnotes, preTagEscapedCharacterTranser];
 
   let preprocessContent = content.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
   if (disableAdvance) {
