617ce9593a29447060da5455c73439349bff7665
Gerald Du <frozenmouse@gmail.com>
2018-03-28 10:07:47 +0800

    修改高價釋股公司之範圍
    此修改依照 https://acgn-stock.com/ruleDiscuss/view/uP6GcD8te5ui8CGgW 中
    「修改高價釋股公司之範圍」的決議。

--- server/functions/company/updateHighPriceThreshold.js
+++ server/functions/company/updateHighPriceThreshold.js
@@ -7,7 +7,14 @@ export function updateHighPriceThreshold() {
   debug.log('updateHighPriceThreshold');
 
   const companyCount = dbCompanies.find({ isSeal: false }).count();
-  const thresholdPosition = Math.floor(companyCount * 0.05);
+
+  if (companyCount === 0) {
+    dbVariables.set('highPriceThreshold', Infinity);
+
+    return;
+  }
+
+  const thresholdPosition = Math.floor(companyCount * (14 - 1.5 * Math.log(companyCount)) / 100);
 
   const companyData = dbCompanies.findOne({ isSeal: false }, {
     fields: { lastPrice: 1 },
