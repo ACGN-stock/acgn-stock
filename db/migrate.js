e549bbd0e02561376d6883333bdc3c4f4940fad9
Gerald Du <frozenmouse@gmail.com>
2018-04-13 01:41:00 +0800

    重構 & 整理部分程式碼
    1. 釋股與參考價更新的 counter 以新的事件排程機制取代，並廢除其 counter 的設定，改用實際毫秒數設定
    2. 將經理人選舉與亂鬥攻擊列表產生的流程分開，並新增亂鬥封關的時間設定以便未來獨立調整
    3. 經理人選舉、亂鬥攻擊列表產生、每日發薪等動作，現在將使用新的事件排程機制
    4. 廢棄 counterManager
    5. 微調部分程式碼的排版

--- db/migrate.js
+++ db/migrate.js
@@ -1452,6 +1452,43 @@ if (Meteor.isServer) {
     }
   });
 
+  Migrations.add({
+    version: 23,
+    name: 'eventSchedules - replace counter-based triggers & schedule season.electManager and arena.joinEnded',
+    up() {
+      const { intervalTimer } = Meteor.settings.public;
+
+      // 將 counter 處理的週期性事件轉換為排程事件
+      const counterToEventMap = {
+        checkChairmanCounter: 'company.checkChairman',
+        recordListPriceCounter: 'company.recordListPrice',
+        releaseStocksForHighPriceCounter: 'company.releaseStocksForHighPrice',
+        releaseStocksForNoDealCounter: 'company.releaseStocksForNoDeal'
+      };
+
+      Object.entries(counterToEventMap).forEach(([counterId, eventId]) => {
+        const counterValue = dbVariables.get(counterId);
+        if (counterValue) {
+          dbVariables.remove(counterId);
+          dbEventSchedules.upsert(eventId, { $set: { scheduledAt: new Date(Date.now() + counterValue * intervalTimer) } });
+        }
+      });
+
+      // 將經理人選舉由 season 的 electTime 轉換為排程事件
+      const currentSeason = dbSeason.findOne({}, { sort: { beginDate: -1 } });
+      if (currentSeason && currentSeason.electTime) {
+        dbEventSchedules.upsert('season.electManager', { $set: { scheduledAt: currentSeason.electTime } });
+      }
+      dbSeason.update({}, { $unset: { electTime: 1 } }, { multi: true });
+
+      // 亂鬥的報名封關事件
+      const currentArena = dbArena.findOne({}, { sort: { beginDate: -1 } });
+      if (currentArena) {
+        dbEventSchedules.upsert('arena.joinEnded', { $set: { scheduledAt: currentArena.joinEndDate } });
+      }
+    }
+  });
+
   Meteor.startup(() => {
     Migrations.migrateTo('latest');
   });
--- server/startup/initializeEventSchedules.js
+++ server/startup/initializeEventSchedules.js
@@ -1,11 +1,88 @@
 import { Meteor } from 'meteor/meteor';
+import { _ } from 'underscore';
 
+import { checkChairman } from '/server/functions/company/checkChairman';
+import { recordListPrice, updateRecordListPricePeriod } from '/server/functions/company/recordListPrice';
+import { releaseStocksForHighPrice, updateReleaseStocksForHighPricePeriod } from '/server/functions/company/releaseStocksForHighPrice';
+import { releaseStocksForNoDeal, updateReleaseStocksForNoDealPeriod } from '/server/functions/company/releaseStocksForNoDeal';
+import { sellFscStocks } from '/server/functions/company/sellFscStocks';
+import { electManager } from '/server/functions/company/electManager';
 import { replenishProducts } from '/server/functions/product/replenishProducts';
 import { checkVipLevels } from '/server/functions/vip/checkVipLevels';
 import { computeArenaAttackSequences } from '/server/functions/arena/computeArenaAttackSequences';
 import { eventScheduler } from '/server/imports/utils/eventScheduler';
+import { paySalaryAndCheckTax } from '../paySalaryAndCheckTax';
 
 Meteor.startup(() => {
+  // 董事長檢查
+  eventScheduler.defineRecurringEvent('company.checkChairman', {
+    onTriggered() {
+      checkChairman();
+    },
+    nextScheduledAt() {
+      return Date.now() + Meteor.settings.public.checkChairmanInterval;
+    }
+  });
+
+  // 參考價更新
+  eventScheduler.defineRecurringEvent('company.recordListPrice', {
+    onTriggered() {
+      updateRecordListPricePeriod();
+      recordListPrice();
+      sellFscStocks(); // 參考價更新同時賣出金管會持股
+      replenishProducts(); // 參考價更新同時補貨
+    },
+    nextScheduledAt() {
+      const { min: intervalMin, max: intervalMax } = Meteor.settings.public.recordListPriceInterval;
+
+      return Date.now() + _.random(intervalMin, intervalMax);
+    }
+  });
+
+  // 高價釋股
+  eventScheduler.defineRecurringEvent('company.releaseStocksForHighPrice', {
+    onTriggered() {
+      updateReleaseStocksForHighPricePeriod();
+      releaseStocksForHighPrice();
+    },
+    nextScheduledAt() {
+      const { min: intervalMin, max: intervalMax } = Meteor.settings.public.releaseStocksForHighPriceInterval;
+
+      return Date.now() + _.random(intervalMin, intervalMax);
+    }
+  });
+
+  // 低量釋股
+  eventScheduler.defineRecurringEvent('company.releaseStocksForNoDeal', {
+    onTriggered() {
+      updateReleaseStocksForNoDealPeriod();
+      releaseStocksForNoDeal();
+    },
+    nextScheduledAt() {
+      const { min: intervalMin, max: intervalMax } = Meteor.settings.public.releaseStocksForNoDealInterval;
+
+      return Date.now() + _.random(intervalMin, intervalMax);
+    }
+  });
+
+  // 每日全域事件
+  eventScheduler.defineRecurringEvent('global.daily', {
+    onTriggered() {
+      console.log('event triggered: global.daily');
+      paySalaryAndCheckTax();
+    },
+    nextScheduledAt() {
+      // 下一天的 00:00 UTC（台灣時間早上八點）
+      return new Date(Date.now() + 24 * 60 * 60 * 1000).setUTCHours(0, 0, 0, 0);
+    }
+  });
+
+  // 季度經理人選舉
+  eventScheduler.setEventCallback('season.electManager', () => {
+    console.log('event triggered: season.electManager');
+    electManager();
+  });
+
   // 產品最後出清
   eventScheduler.setEventCallback('product.finalSale', () => {
     console.log('event triggered: product.finalSale');
