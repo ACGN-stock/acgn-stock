102e48fee2c65be47ffed30ed6836ab12d2b4f1a
Gerald Du <frozenmouse@gmail.com>
2018-05-26 20:03:14 +0800

    eslint

--- client/violation/violatorDisplay.js
+++ client/violation/violatorDisplay.js
@@ -1,11 +1,4 @@
-import { _ } from 'meteor/underscore';
-import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
-import { FlowRouter } from 'meteor/kadira:flow-router';
-
-import { categoryDisplayName, categoryMap, dbViolationCases } from '/db/dbViolationCases';
-import { inheritUtilForm } from '../utils/form';
-import { alertDialog } from '../layout/alertDialog';
 
 Template.violatorDisplay.helpers({
   isViolatorType(type) {
