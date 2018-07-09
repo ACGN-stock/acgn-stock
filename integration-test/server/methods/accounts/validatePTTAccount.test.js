cfa6c221445c9e02d7e60332a523d18b3787a5e2
Gerald Du <frozenmouse@gmail.com>
2018-07-10 01:54:10 +0800

    修正 PTT 帳號驗證時，可以用另一個部分符合使用者名稱的帳號來破解驗證程序的問題
    感謝 Darkpi 回報此漏洞
    
    前置條件
    1. 使用者 `foobar` 已完成驗證存在於股市系統
    2. 使用者 `foobar2` 已完成驗證存在於股市系統，並且不在驗證中狀態
    3. PTT 系統存在另一個使用者 `foobar2`，並且有權限在指定的驗證文章進行推文
    
    操作步驟
    1. 使用不正確的密碼，在股市系統中試圖登入 `foobar`
    2. 使用重設密碼功能，使 `foobar` 帳號進入驗證程序，並取得正確的驗證碼
    3. 在 PTT 的指定文章中，使用 `foobar2` 使用者進行推文，內容包含正確的驗證碼
    4. 當 `validatePTTAccount` 被執行時，`foobar` 的密碼將被重設為第 1. 步提供的密碼
    
    判斷推文資料必須完全符合 validatingUser 的 username 資料，可以解決上述問題。

--- integration-test/server/methods/accounts/validatePTTAccount.test.js
+++ integration-test/server/methods/accounts/validatePTTAccount.test.js
@@ -83,6 +83,7 @@ describe('method validatePTTAccount', function() {
 
   describe('with pending validation', function() {
     beforeEach(function() {
+      // 帳號正在驗證中（either 新帳號申請 or 重設密碼）
       dbValidatingUsers.insert({
         username,
         password: newPassword,
@@ -150,6 +151,47 @@ describe('method validatePTTAccount', function() {
           const { _id: userId } = Meteor.users.findOne({ username });
           Accounts.setPassword.must.have.been.calledWith(userId, newPassword);
         });
+    });
+
+    it('should not affect the user that only partially matches the username', function() {
+      // 真帳號已存在於系統（完成驗證），但正在等待驗證（重設密碼）
+      Accounts.createUser({
+        username,
+        password: currentPassword,
+        profile: {
+          name: username,
+          validateType: 'PTT'
+        }
+      });
+
+      // 存在假帳號，其使用者名稱包含真帳號
+      const fakeUsername = `${username}_Fake`;
+
+      // 假帳號同時存在於系統（完成驗證），且並不處於待驗證狀態
+      Accounts.createUser({
+        username: fakeUsername,
+        password: currentPassword,
+        profile: {
+          name: fakeUsername,
+          validateType: 'PTT'
+        }
+      });
+
+      // 取得真帳號的驗證碼後用假帳號推文
+      HTTP.get.returns({
+        content: `
+          <div class="push">
+            <span class="push-userid">${fakeUsername}</span>
+            <span class="push-content">: ${correctValidateCode}</span>
+          </div>
+        `
+      });
+
+      return validatePTTAccount(fakeUsername).then(() => {
+        // 真帳號的密碼不應被重設
+        const { _id: userId } = Meteor.users.findOne({ username });
+        Accounts.setPassword.must.have.not.been.calledWith(userId, newPassword);
+      });
     });
   });
 });
