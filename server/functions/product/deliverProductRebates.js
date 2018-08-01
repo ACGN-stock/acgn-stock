43d145a53aef0f8a648c720d1288b88001bea245
SoftwareSing <SoftwareSing@gmail.com>
2018-08-02 01:46:15 +0800

    重構 deliverProductRebates()
    重構 deliverProductRebates() ，將查找 rebateList 的部分移出，並將計算 rebate 的部分獨立成 computeRebate() 方便修改

--- server/functions/product/deliverProductRebates.js
+++ server/functions/product/deliverProductRebates.js
@@ -7,37 +7,7 @@ import { getCurrentSeason } from '/db/dbSeason';
 
 // 發放產品滿額回饋金給使用者
 export function deliverProductRebates() {
-  const { divisorAmount, deliverAmount } = Meteor.settings.public.productRebates;
-
-  const { _id: seasonId } = getCurrentSeason();
-
-  const rebateList = dbUserOwnedProducts
-    .aggregate([ {
-      $match: { seasonId }
-    }, {
-      $lookup: {
-        from: 'companies',
-        localField: 'companyId',
-        foreignField: '_id',
-        as: 'companyData'
-      }
-    }, {
-      $unwind: '$companyData'
-    }, {
-      $match: { 'companyData.isSeal': false }
-    }, {
-      $group: {
-        _id: { userId: '$userId', companyId: '$companyId' },
-        totalCost: { $sum: { $multiply: ['$amount', '$price'] } }
-      }
-    }, {
-      $project: {
-        rebate: { $multiply: [deliverAmount, { $floor: { $divide: ['$totalCost', divisorAmount] } } ] }
-      }
-    }, {
-      $match: { rebate: { $gt: 0 } }
-    } ]);
-
+  const rebateList = getRebateList();
   if (_.isEmpty(rebateList)) {
     return;
   }
@@ -63,4 +33,49 @@ export function deliverProductRebates() {
 
   Meteor.wrapAsync(userBulk.execute, userBulk)();
   Meteor.wrapAsync(logBulk.execute, logBulk)();
+}
+
+function getRebateList() {
+  const costList = getCostList();
+  const rebateList = [];
+  costList.forEach(({ _id, totalCost }) => {
+    const rebate = computeRebate(totalCost);
+    if (rebate > 0) {
+      rebateList.push({ _id, rebate });
+    }
+  });
+
+  return rebateList;
+}
+
+function getCostList() {
+  const { _id: seasonId } = getCurrentSeason();
+  const costList = dbUserOwnedProducts
+    .aggregate([ {
+      $match: { seasonId }
+    }, {
+      $lookup: {
+        from: 'companies',
+        localField: 'companyId',
+        foreignField: '_id',
+        as: 'companyData'
+      }
+    }, {
+      $unwind: '$companyData'
+    }, {
+      $match: { 'companyData.isSeal': false }
+    }, {
+      $group: {
+        _id: { userId: '$userId', companyId: '$companyId' },
+        totalCost: { $sum: { $multiply: ['$amount', '$price'] } }
+      }
+    } ]);
+
+  return costList;
+}
+
+function computeRebate(totalCost) {
+  const { divisorAmount, deliverAmount } = Meteor.settings.public.productRebates;
+
+  return deliverAmount * Math.floor(totalCost / divisorAmount);
 }
