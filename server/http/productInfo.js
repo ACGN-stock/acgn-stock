3ce84db7e4d06e3375adb90ee5bc48b349f1e2b8
Gerald Du <frozenmouse@gmail.com>
2018-08-16 20:35:45 +0800

    移除 Google 帳號的名稱顯示 & 重整 ajax 相關的 server 端程式碼
    1. `/userInfo` 發佈 Google 帳號玩家資訊時，不再透過 Google API 取得顯示名稱，
        一律以 email 作為玩家名稱顯示，避免因自訂名稱造成辨識困難
    2. 調整 `/userInfo`、`/productInfo` 與 `/companyInfo`，簡化部分寫法，
        在該 id 不存在時以 404 回應而非丟回單純空資料

--- server/http/productInfo.js
+++ server/http/productInfo.js
@@ -6,29 +6,27 @@ import { dbProducts } from '/db/dbProducts';
 import { debug } from '/server/imports/utils/debug';
 
 // 以Ajax方式發布產品名稱、連結
-WebApp.connectHandlers.use(function(req, res, next) {
+WebApp.connectHandlers.use('/productInfo', function(req, res) {
   debug.log('connectHandlers productInfo');
-  const parsedUrl = url.parse(req.url);
-  if (parsedUrl.pathname === '/productInfo') {
-    const query = querystring.parse(parsedUrl.query);
-    const productId = query.id;
-    const productData = dbProducts.findOne(productId, {
-      fields: {
-        productName: 1,
-        url: 1,
-        type: 1,
-        companyId: 1
-      }
-    });
-    if (productData) {
-      res.setHeader('Cache-Control', 'public, max-age=604800');
-      res.end(JSON.stringify(productData));
+  const { query } = url.parse(req.url);
+  const { id: productId } = querystring.parse(query);
+
+  const productData = dbProducts.findOne(productId, {
+    fields: {
+      productName: 1,
+      url: 1,
+      type: 1,
+      companyId: 1
     }
-    else {
-      res.end('');
-    }
+  });
+
+  if (! productData) {
+    res.statusCode = 404;
+    res.end();
+
+    return;
   }
-  else {
-    next();
-  }
+
+  res.setHeader('Cache-Control', 'public, max-age=604800');
+  res.end(JSON.stringify(productData));
 });
--- server/http/userInfo.js
+++ server/http/userInfo.js
@@ -1,57 +1,34 @@
-import { Meteor } from 'meteor/meteor';
 import { WebApp } from 'meteor/webapp';
-import { HTTP } from 'meteor/http';
 import url from 'url';
 import querystring from 'querystring';
 
 import { dbUserArchive } from '/db/dbUserArchive';
 import { debug } from '/server/imports/utils/debug';
 
-// 以Ajax方式發布使用者名稱
-WebApp.connectHandlers.use(function(req, res, next) {
+// 以Ajax方式發布使用者資訊
+WebApp.connectHandlers.use('/userInfo', (req, res) => {
   debug.log('connectHandlers userName');
-  const parsedUrl = url.parse(req.url);
-  if (parsedUrl.pathname === '/userInfo') {
-    const query = querystring.parse(parsedUrl.query);
-    const userId = query.id;
-    const userData = dbUserArchive.findOne(userId, {
-      fields: {
-        name: 1,
-        status: 1,
-        validateType: 1
-      }
-    });
-    if (userData) {
-      if (userData.status === 'registered') {
-        res.setHeader('Cache-Control', 'public, max-age=604800');
-      }
-      if (userData.validateType === 'Google') {
-        try {
-          const accessToken = Meteor.users.findOne(userId, {
-            fields: {
-              'services.google.accessToken': 1
-            }
-          }).services.google.accessToken;
-          /* eslint-disable camelcase */
-          const response = HTTP.get('https://www.googleapis.com/oauth2/v1/userinfo', {
-            params: {
-              access_token: accessToken
-            }
-          });
-          /* eslint-enable camelcase */
-          userData.name = response.data.name;
-        }
-        catch (e) {
-          userData.name = userData.name;
-        }
-      }
-      res.end(JSON.stringify(userData));
+  const { query } = url.parse(req.url);
+  const { id: userId } = querystring.parse(query);
+
+  const userData = dbUserArchive.findOne(userId, {
+    fields: {
+      name: 1,
+      status: 1,
+      validateType: 1
     }
-    else {
-      res.end('');
-    }
+  });
+
+  if (! userData) {
+    res.statusCode = 404;
+    res.end();
+
+    return;
   }
-  else {
-    next();
+
+  if (userData.status === 'registered') {
+    res.setHeader('Cache-Control', 'public, max-age=604800');
   }
+
+  res.end(JSON.stringify(userData));
 });
