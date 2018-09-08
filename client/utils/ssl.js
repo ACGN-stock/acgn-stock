8162bd4a5556fa2f22a40c9a1aa42a9fcfdab57f
mrbigmouth <b92101007@gmail.com>
2018-09-08 13:13:43 +0800

    加入domain/port檢測避免玩家連測試機時誤連正式機。

--- client/utils/ssl.js
+++
@@ -1,5 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-
-if (Meteor.isProduction && Meteor.settings.public.debugMode === false && location.protocol === 'http:') {
-  location.href = location.href.replace('http:', 'https:');
-}
