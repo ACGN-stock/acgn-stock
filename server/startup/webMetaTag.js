65548e96de85f1a2c3e746a37be9a68419eb6337
SoftwareSing <SoftwareSing@gmail.com>
2018-09-17 16:00:22 +0800

    調整meta tag的內容
    讓 site name 放在共通的tag
    並加入 twitter:card 設定
    
    調整公司資訊的顯示

--- server/startup/webMetaTag.js
+++ server/startup/webMetaTag.js
@@ -7,39 +7,59 @@ onPageLoad((sink) => {
   const companyId = extractCompanyId(pathname);
   const companyData = companyId ? getCompanyData(companyId) : null;
 
+  let metaTag = getCommonMetaTag();
   if (companyData) {
-    appendCompanyMetaTag(sink, companyData);
+    metaTag += getCompanyMetaTag(companyData);
   }
   else {
-    appendDefaultMetaTag(sink);
+    metaTag += getDefaultMetaTag();
   }
+  sink.appendToHead(metaTag);
 });
 
-function appendDefaultMetaTag(sink) {
-  sink.appendToHead(createMetaTag('og:title', 'ACGN 股票交易市場'));
-  sink.appendToHead(createMetaTag('og:description', '漲停!!'));
-  sink.appendToHead(createMetaTag('og:image', '/ms-icon-310x310.png'));
+function getCommonMetaTag() {
+  let metaTag = '';
+  metaTag += createMetaName('twitter:card', 'summary');
+  metaTag += createMetaProperty('og:site_name', 'acgn-stock');
+
+  return metaTag;
 }
 
-function appendCompanyMetaTag(sink, companyData) {
-  const { companyName, pictureSmall } = companyData;
-  sink.appendToHead(createMetaTag('og:site_name', 'acgn-stock'));
-  sink.appendToHead(createMetaTag('og:title', companyName));
-  sink.appendToHead(createMetaTag('og:image', pictureSmall));
-  sink.appendToHead(createMetaTag('og:description', createCompanyDescription(companyData)));
+function getDefaultMetaTag() {
+  let metaTag = '';
+  metaTag += createMetaProperty('og:title', 'ACGN 股票交易市場');
+  metaTag += createMetaProperty('og:image', '/ms-icon-310x310.png');
+  metaTag += createMetaProperty('og:description', '漲停!!');
+
+  return metaTag;
 }
+
+function getCompanyMetaTag(companyData) {
+  let metaTag = '';
+  const { companyName, pictureSmall } = companyData;
+  metaTag += createMetaProperty('og:title', companyName);
+  metaTag += createMetaProperty('og:image', pictureSmall);
+  metaTag += createMetaProperty('og:description', createCompanyDescription(companyData));
+
+  return metaTag;
+}
+
 
 function createCompanyDescription({ listPrice, capital, totalValue, description }) {
-  return `價格: ${listPrice} ——— 市值: ${capital} ——— 資本額: ${totalValue}
+  return `價格: ${listPrice} ｜ 市值: ${capital} ｜ 資本額: ${totalValue}
 
     ${description}
   `;
 }
 
-
-function createMetaTag(property, content) {
+function createMetaProperty(property, content) {
   return `<meta property="${property}" content="${content}" />`;
 }
+
+function createMetaName(name, content) {
+  return `<meta name="${name}" content="${content}" />`;
+}
+
 
 function getCompanyData(companyId) {
   return dbCompanies.findOne({ _id: companyId },
