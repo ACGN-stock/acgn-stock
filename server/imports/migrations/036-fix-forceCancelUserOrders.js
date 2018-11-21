e99730be26abc535bbc988ac47c499f633fff65c
SoftwareSing <SoftwareSing@gmail.com>
2018-11-22 03:44:30 +0800

    修正Migration 036
    由於 dbDirectors 用的是mongo原生的 ObjectID ，拿出來後不能直接丟回去查，必須另建 ObjectID 再丟回去

--- server/imports/migrations/036-fix-forceCancelUserOrders.js
+++ server/imports/migrations/036-fix-forceCancelUserOrders.js
@@ -1,27 +1,28 @@
+import { MongoInternals } from 'meteor/mongo';
+
 import { defineMigration } from '/server/imports/utils/defineMigration';
 import { executeBulksSync } from '/server/imports/utils/executeBulksSync';
 import { dbDirectors } from '/db/dbDirectors';
 
 defineMigration({
   version: 36,
-  name: 'fix forceCancelUserOrders',
+  name: 'fix dbDirectors have two same user director data in one company',
   up() {
-    const directorsBulk = dbDirectors.rawCollection().initializeUnorderedBulkOp();
-    const directors = dbDirectors.find({}).fetch();
+    const ObjectID = MongoInternals.NpmModule.ObjectID;
+    const directorsBulk = dbDirectors.rawCollection().initializeOrderedBulkOp();
     const directorsMap = {};
-    while (directors.length > 0) {
-      const director = directors.pop();
+    dbDirectors.find({}).forEach((director) => {
       const mapKey = director.userId + director.companyId;
       if (directorsMap[mapKey]) {
-        directorsBulk.find({ _id: director._id }).remove();
+        directorsBulk.find({ _id: new ObjectID(director._id._str) }).remove();
         directorsBulk
-          .find({ _id: directorsMap[mapKey] })
+          .find({ _id: new ObjectID(directorsMap[mapKey]._str) })
           .updateOne({ $inc: { stocks: director.stocks } });
       }
       else {
         directorsMap[mapKey] = director._id;
       }
-    }
+    });
     executeBulksSync(directorsBulk);
   }
 });
