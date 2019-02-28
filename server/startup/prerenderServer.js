da40f7a8784875946f09c7ad9f9983bcb523bc34
SoftwareSing <SoftwareSing@gmail.com>
2019-02-28 16:34:30 +0800

    設置等待時間讓prerender有足夠的時間render
    設定3秒的等待時間，讓網頁能跑完整，也避免過於快速的做下一次請求造成伺服器負擔

--- server/startup/prerenderServer.js
+++ server/startup/prerenderServer.js
@@ -2,7 +2,10 @@ import prerender from 'prerender';
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  const prerenderServer = prerender({ port: Meteor.settings.public.prerenderServer.port });
+  const prerenderServer = prerender({
+    port: Meteor.settings.public.prerenderServer.port,
+    waitAfterLastRequest: 3000
+  });
   prerenderServer.start();
   process.env.DISABLE_LOGGING = true;
 });
