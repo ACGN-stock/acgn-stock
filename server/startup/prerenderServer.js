c20c70cd29b366b2ef1adb3b8b4f570ef8175b14
SoftwareSing <SoftwareSing@gmail.com>
2019-02-28 20:05:20 +0800

    增加 production mode
    在 production 設定為 true 的情況下才會開啟 prerender server

--- server/startup/prerenderServer.js
+++ server/startup/prerenderServer.js
@@ -2,6 +2,10 @@ import prerender from 'prerender';
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
+  if (! Meteor.settings.public.production) {
+    return;
+  }
+
   const prerenderServer = prerender({
     port: Meteor.settings.public.prerenderServer.port,
     waitAfterLastRequest: 3000
