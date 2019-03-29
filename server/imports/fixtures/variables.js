6557fed0606e34f60334c1e2e6df64ac5bbe0f0a
SoftwareSing <SoftwareSing@gmail.com>
2019-03-29 12:53:26 +0800

    修正預設的驗證文章位置

--- server/imports/fixtures/variables.js
+++ server/imports/fixtures/variables.js
@@ -1,9 +1,9 @@
 import { dbVariables } from '/db/dbVariables';
 import { updateFoundationVariables } from '/server/functions/foundation/updateFoundationVariables';
 
-dbVariables.setIfNotFound('validateUserUrl', 'https://www.ptt.cc/bbs/C_Chat/M.1501484745.A.B15.html');
-dbVariables.setIfNotFound('validateUserBoardName', 'C_Chat');
-dbVariables.setIfNotFound('validateUserAID', '#1PVjR9iL');
+dbVariables.setIfNotFound('validateUserUrl', 'https://www.ptt.cc/bbs/ACGN_stock/M.1508993391.A.381.html');
+dbVariables.setIfNotFound('validateUserBoardName', 'ACGN_stock');
+dbVariables.setIfNotFound('validateUserAID', '#1PyMblE1');
 
 // 初始化新創相關變數
 if (! dbVariables.has('foundation.minInvestorCount') || ! dbVariables.has('foundation.minAmountPerInvestor')) {
