0a0eed9b7e352aad125412bd034e7d7593c43b61
SoftwareSing <SoftwareSing@gmail.com>
2019-04-07 03:40:43 +0800

    嘗試用bulk跑Migration

--- server/imports/migrations/043-rename-name-to-companyName-in-companyArchive.js
+++ server/imports/migrations/043-rename-name-to-companyName-in-companyArchive.js
@@ -1,14 +1,15 @@
 import { defineMigration } from '/server/imports/utils/defineMigration';
 import { dbCompanyArchive } from '/db/dbCompanyArchive';
+import { executeBulksSync } from '/server/imports/utils/executeBulksSync';
 
 defineMigration({
   version: 43,
   name: 'rename name to companyName in companyArchive',
   async up() {
     await dbCompanyArchive.rawCollection().dropIndex({ name: 1 });
-    await dbCompanyArchive.rawCollection().update({}, {
-      $rename: { name: 'companyName' }
-    }, { multi: true });
+    const companyArchiveBulk = dbCompanyArchive.rawCollection().initializeUnorderedBulkOp();
+    companyArchiveBulk.find({}).update({ $rename: { name: 'companyName' } });
+    executeBulksSync(companyArchiveBulk);
     await dbCompanyArchive.rawCollection().createIndex({ companyName: 1 }, { unique: true });
   }
 });
