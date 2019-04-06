b63cd71b266d4bc6d980fac120ea7e34fbafce74
SoftwareSing <SoftwareSing@gmail.com>
2019-04-07 02:14:33 +0800

    Revert "修正 Migration 043"
    This reverts commit 8e56a2f0b68a7b9966ef1b524fab535e45f9c949.

--- server/imports/migrations/043-rename-name-to-companyName-in-companyArchive.js
+++ server/imports/migrations/043-rename-name-to-companyName-in-companyArchive.js
@@ -6,7 +6,7 @@ defineMigration({
   name: 'rename name to companyName in companyArchive',
   async up() {
     await dbCompanyArchive.rawCollection().dropIndex({ name: 1 });
-    await dbCompanyArchive.update({}, {
+    await dbCompanyArchive.rawCollection().update({}, {
       $rename: { name: 'companyName' }
     }, { multi: true });
     await dbCompanyArchive.rawCollection().createIndex({ companyName: 1 }, { unique: true });
