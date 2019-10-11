84d717e5d85b536f77f1958bd7de23f939bb6a58
SoftwareSing <softwaresing@gmail.com>
2019-10-11 23:33:47 +0800

    重構 recordListPrice

--- server/functions/company/recordListPrice.js
+++ server/functions/company/recordListPrice.js
@@ -1,9 +1,9 @@
-import { _ } from 'meteor/underscore';
 import { Meteor } from 'meteor/meteor';
 
 import { resourceManager } from '/server/imports/threading/resourceManager';
 import { dbCompanies } from '/db/dbCompanies';
 import { dbVariables } from '/db/dbVariables';
+import { executeBulksSync } from '/server/imports/utils/executeBulksSync';
 
 export function updateRecordListPricePeriod() {
   const { min: intervalMin, max: intervalMax } = Meteor.settings.public.recordListPriceInterval;
@@ -16,27 +16,20 @@ export function updateRecordListPricePeriod() {
 // 更新參考價（與參考市值）
 export function recordListPrice() {
   resourceManager.requestPromise('recordListPrice', ['allCompanyOrders'], (release) => {
-    const companyUpdateModifierMap = dbCompanies
+    const companyBulk = dbCompanies.rawCollection().initializeUnorderedBulkOp();
+    dbCompanies
       .find({ isSeal: false }, { fields: { lastPrice: 1, totalRelease: 1 } })
       .fetch()
-      .reduce((obj, { _id: companyId, lastPrice, totalRelease }) => {
-        obj[companyId] = {
-          $set: {
+      .forEach(({ _id: companyId, lastPrice, totalRelease }) => {
+        companyBulk
+          .find({ _id: companyId })
+          .updateOne({ $set: {
             listPrice: lastPrice,
             totalValue: lastPrice * totalRelease
-          }
-        };
-
-        return obj;
-      }, {});
-
-    if (! _.isEmpty(companyUpdateModifierMap)) {
-      const companyBulk = dbCompanies.rawCollection().initializeUnorderedBulkOp();
-      Object.entries(companyUpdateModifierMap).forEach(([companyId, modifier]) => {
-        companyBulk.find({ _id: companyId }).updateOne(modifier);
+          } });
       });
-      Meteor.wrapAsync(companyBulk.execute, companyBulk)();
-    }
+
+    executeBulksSync(companyBulk);
 
     release();
   });
